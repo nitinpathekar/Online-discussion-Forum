3fa45a9d238df160a094dad4c391b098f606946a
Nitin <nitinsardarpathekar@gmail.com>
2025-12-09 17:15:09 +0530

    change name

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
         <Typography variant="subtitle1">
           Don't forget to star the{" "}
           <a
-            href="https://github.com/ihtasham42/social-media-app"
+            href="https://github.com/nitinpathekar/online-discussion-forum"
             target="_blank"
           >
             Repo
